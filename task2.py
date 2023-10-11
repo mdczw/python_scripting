9c9e76e47481e99b2938a5f69e37aca290ad61d0
Rita <mdyczewska@griddymanics.com>
2023-10-11 13:38:04 +0200

    Script accepts a JSON file and creates a survey

--- task2.py
+++ task2.py
@@ -1,24 +1,74 @@
 #!/usr/bin/env python3
 
 import http.client
+import sys
+import json
 
-conn = http.client.HTTPSConnection("api.surveymonkey.com")
 
-#with open('qa_task2.json') as f:
-#    payload = f.read()
+def get_json_file():
+    input_file = '' 
+    while input_file == '':
+        input_file = input("Enter a JSON file:\n")
+    try:
+        f = open(input_file)
+    except FileNotFoundError as err:
+        print(f"Error: {err}")
+        sys.exit(1)
+    else:
+        with f:
+            json_file = json.load(f)
+    return json_file
 
-payload = "{\"title\":\"New Survey\",\"nickname\":\"My Survey\",\"language\":\"en\",\"buttons_text\":{\"next_button\":\"string\",\"prev_button\":\"string\",\"exit_button\":\"string\",\"done_button\":\"string\"},\"custom_variables\":{},\"footer\":true}"
 
-headers = {
+def create_survey_api(json_file):
+    try:
+        survey = {
+            "title": list(json_file.keys())[0],
+            "pages": []
+        }
+        for page_name, page_data in json_file[list(json_file.keys())[0]].items():
+            page = {
+                "title": page_name,
+                "questions": []
+                }
+            for question_name, question_data in page_data.items():
+                question = {
+                    "headings": [{"heading": question_data['Description']}],
+                    "family": "single_choice",
+                    "subtype": "vertical",
+                    "answers": {"choices": []}
+                }
+                for answer_name in question_data['Answers']:
+                    answer = {
+                        "text": answer_name
+                    }
+                    question["answers"]["choices"].append(answer)
+                page["questions"].append(question)
+            survey["pages"].append(page)
+    
+    except:
+        print("Enter a correct JSON file")
+        print('Example:\n{ "Survey_Name": { "Page_Name": { "Question1": { "Description" : "Description of question 1", "Answers" : [ "Answer1", "Answer2", "Answer3" ] }, "Question2": { "Description" : "Description of question 2", "Answers" : [ "Answer1", "Answer2", "Answer3" ] }, "Question3": { "Description" : "Description of question 3", "Answers" : [ "Answer1", "Answer2", "Answer3" ] } } } }')
+        sys.exit(1)
+    else:
+        return json.dumps(survey)
+
+
+def surveymonkey_connaction(access_token, payload):
+    conn = http.client.HTTPSConnection("api.surveymonkey.com")
+    headers = {
     'Content-Type': "application/json",
     'Accept': "application/json",
-    'Authorization': "Bearer EiwSAO2.L6DsGStNzQCccy9q7tCB4x22swJmYJQprzRghUnUGTVFX5CUCpv45Kvh1olFm08ZEMMpmrdfTqpDaAEUxfVs78YDOJfORSY-pS7TKpsE2npQU7tzhVACF.5Z"
+    'Authorization': f"Bearer {access_token}"
     }
 
-conn.request("POST", "/v3/surveys", payload, headers)
+    conn.request("POST", "/v3/surveys", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
 
-res = conn.getresponse()
-data = res.read()
+access_token = "EiwSAO2.L6DsGStNzQCccy9q7tCB4x22swJmYJQprzRghUnUGTVFX5CUCpv45Kvh1olFm08ZEMMpmrdfTqpDaAEUxfVs78YDOJfORSY-pS7TKpsE2npQU7tzhVACF.5Z"
 
-print(data.decode("utf-8"))
-
+json_file = get_json_file()
+payload = create_survey_api(json_file)
+surveymonkey_connaction(access_token, payload)
